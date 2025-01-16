414e735f7810246d53f0e373b24523dfabb5e093
DevoutElk <eliassver01@gmail.com>
2025-01-16 12:37:37 +0100

    levantar x script

---
+++ levantar-X.scripts.sh
@@ -0,0 +1,11 @@
+pidof mate-session > ~/Escritorio/mate-session.pid
+
+read -p "Introduce el número de sesión: " session_number
+
+# Verifica si ya hay un proceso relacionado con el número de sesión
+if pgrep -af "mate-session -- :$session_number" | grep -q "startx"; then
+    echo "El proceso con sesión :$session_number ya está en ejecución. Abortando"
+else
+    echo "Iniciando sesión :$session_number..."
+    startx /usr/bin/mate-session -- :$session_number
+fi
